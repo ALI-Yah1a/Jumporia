2d2e656ba660708798755cb14b56add7f6b670e9
ALI-Yah1a <aliyahia1230123@gmail.com>
2026-03-16 23:56:37 +0200

    Finishing the full main menu

---
+++ UI/button_sfx.gd
@@ -0,0 +1,40 @@
+extends AudioStreamPlayer
+
+func play_click():
+	$".".play()
+
+func _on_button_pressed() -> void:
+	play_click()
+
+
+
+func _on_button_2_pressed() -> void:
+	play_click()
+
+
+func _on_button_3_pressed() -> void:
+	play_click()
+
+
+func _on_back_pressed() -> void:
+	play_click()
+
+
+func _on_level_button_pressed() -> void:
+	play_click()
+
+
+func _on_level_button_2_pressed() -> void:
+	play_click()
+
+
+func _on_level_button_3_pressed() -> void:
+	play_click()
+
+
+func _on_level_button_4_pressed() -> void:
+	play_click()
+
+
+func _on_level_button_5_pressed() -> void:
+	play_click()
--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,21 +1,26 @@
 extends Control
 @onready var buttons: VBoxContainer = $Buttons
 @onready var options: Panel = $Options
+@onready var level_selection: Panel = $LevelSelection
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	buttons.visible = true
 	options.visible = false
+	level_selection.visible = false
 
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	buttons.visible = false
+	options.visible = false
+	level_selection.visible = true
 
 
 func _on_options_2_pressed() -> void:
 	buttons.visible = false
 	options.visible = true
+	level_selection.visible = false
 
 
 func _on_exit_3_pressed() -> void:
@@ -24,3 +29,23 @@ func _on_exit_3_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	_ready()
+
+
+
+func _on_level_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
+
+
+func _on_level_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/level_2.tscn")
+
+func _on_level_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/level_3.tscn")
+
+
+func _on_level_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/level_4.tscn")
+
+
+func _on_level_button_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/level_5.tscn")
